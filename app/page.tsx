6fc9859854f69ea6f11bea4fa4f98cfdc6319a56
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2024-05-11 16:05:55 +0100

    fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import { NavBar } from "@/components/nav-bar";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
-      <div>
-        <NavBar/>
-      </div>
-      <div className="my-20 px-4 lg:px-20">
-        </div>
+      Welcome to Camer tourist api
     </div>
   );
 }
